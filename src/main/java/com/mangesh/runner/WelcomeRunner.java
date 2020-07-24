2593ab082aeab2bf6e2b6a6a22ba0b327499e57e
mangesh <mvadkte@gmail.com>
2020-07-24 21:44:20 +0530

    WH-2123 Add One Statement

--- src/main/java/com/mangesh/runner/WelcomeRunner.java
+++ src/main/java/com/mangesh/runner/WelcomeRunner.java
@@ -9,6 +9,7 @@ public class WelcomeRunner implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("WELCOME SPRING BOOT APPLICATION..!!!!");
+		System.out.println("WELCOME MANESH VADKTE..!!!!");
 	}
 
 }
