80edc090ebf216dd70ae27f6b59fbd77c7dd26b9
mangesh <mvadkte@gmail.com>
2020-07-24 21:03:45 +0530

    WR-2121 First Application Structure Created..

---
+++ src/main/java/com/mangesh/runner/WelcomeRunner.java
@@ -0,0 +1,12 @@
+package com.mangesh.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class WelcomeRunner implements CommandLineRunner{
+
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("WELCOME SPRING BOOT APPLICATION..!!!!");
+	}
+
+}
